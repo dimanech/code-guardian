06e810ed79d7d17153bf53a1e38db5508cb07b1d
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2014-08-09 22:47:35 +0300

    ADDED: SCSS support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Deveolped by Dmytro Nechepurenko. Distributed by MIT license.
 
 ## About
-Code guardian is example gulp-based set for code review process.
+Code guardian is example of gulp-based set for code review process.
 Grunt based version you can check [here](https://github.com/dimanech/code-guardian/tree/grunt)
 
 ## Installation
@@ -13,7 +13,13 @@ Requaired: nodejs, npm
 	npm install gulp-csslint
 	npm install map-stream
 
-	cp pre-commit ./.git/hooks/
+	npm install gulp
+	npm install gulp-util
+	gem install scss-lint (https://www.npmjs.org/package/gulp-scss-lint)
+	npm install gulp-scss-lint
+
+	cp ./pre-commit ./.git/hooks/
 
 ## Start manually
-	gulp csslinting
+	gulp css-linting
+	gulp scss-linting
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,56 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	csslint = require('gulp-csslint'),
-	map = require('map-stream');
+	map = require('map-stream'),
+	scsslint = require('gulp-scss-lint'),
+	cssSource = 'css_good/*.css',
+	scssSource = 'css_good/*.scss';
 
-var customReporter = function(file) {
-	gutil.log(gutil.colors.red(file.csslint.errorCount + ' errors in '+ file.relative));
+var scssCustomReporter = function (file) {
+	gutil.log(gutil.colors.magenta(file.scsslint.issues.length + ' errors in ' + file.relative));
 
-	file.csslint.results.forEach(function(result) {
-		gutil.log(gutil.colors.yellow(result.error.line + ': ') + result.error.message);
+	file.scsslint.issues.forEach(function (issue) {
+		gutil.log(gutil.colors.cyan(file.relative + ':' + issue.line) + ' ' + issue.reason)
 	});
-
 };
 
-gulp.task('csslinting', function() {
-	gulp.src('css/*.css')
-		.pipe(csslint('csslintrc.json'))
-		.pipe(csslint.reporter(customReporter))
-		.pipe(map(function (file, cb) {
-			// Notify the callback "true" if it failed on the file
-			cb(!file.csslint.success, file);
-		}));
+var cssCustomReporter = function (file) {
+	gutil.log(gutil.colors.magenta(file.csslint.errorCount + ' errors in ' + file.relative));
 
+	file.csslint.results.forEach(function (result) {
+		gutil.log(gutil.colors.cyan(file.relative + ':' + result.error.line) + ' ' + result.error.message);
+	});
+};
+
+gulp.task('scss-check', function () {
+	gulp.src(scssSource)
+		.pipe(scsslint({
+			'config': 'scsslint.yml',
+			customReport: scssCustomReporter
+		}))
 });
+
+gulp.task('css-check', ['scss-check'], function () {
+	gulp.src(cssSource)
+		.pipe(csslint('csslintrc.json'))
+		.pipe(csslint.reporter(cssCustomReporter))
+});
+
+gulp.task('scss-check-exit', function () {
+	gulp.src(scssSource)
+		.pipe(scsslint({
+			'config': 'scsslint.yml'
+		}))
+		.pipe(scsslint.failReporter());
+});
+
+gulp.task('css-check-exit', ['scss-check-exit'], function () {
+	gulp.src(cssSource)
+		.pipe(csslint('csslintrc.json'))
+		.pipe(csslint.failReporter());
+});
+
+gulp.task('code-guardian', ['scss-check', 'css-check']);
+
+gulp.task('default', ['scss-check-exit', 'css-check-exit']);
 
